dab68814f84fc5d3b10cdaef4f0ebe7796e4bbf9
IceCreeeeam <32411831+Ais-kun@users.noreply.github.com>
2018-02-27 18:47:00 +0900

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,43 +1,25 @@
 import discord
 import asyncio
-import os
-import sys
 
 client = discord.Client()
 
+
 @client.event
 async def on_ready():
-    print('Logged in as')
-    print(client.user.name)
-    print(client.user.id)
-    print('------')
+    print('Login to ' + client.user.name)
+    print('User_ID ' + client.user.id)
+    print('Hello_Discord!')
 
 @client.event
 async def on_message(message):
     # 「おはよう」で始まるか調べる
-    if message.content.startswith("しね"):
+    if message.content.startswith("Hello"):
         # 送り主がBotだった場合反応したくないので
-      for var in range(0, 10):
         if client.user != message.author:
-            # メッセージを書きます
-            m = "FuckYou!!!" + message.author.name
-            n = "FuckYou!!!" + message.author.name
-            o = "FuckYou!!!" + message.author.name
-            # メッセージが送られてきたチャンネルへメッセージを送ります
+            # Write↓
+            m = "Hellooow" + message.author.name
+            # Send to
             await client.send_message(message.channel, m)
-            await client.send_message(message.channel, n)
-            await client.send_message(message.channel, o)
-
-@client.event
-async def on_message(message):
-    if message.content.startswith("こまんどいち"):
-        await client.send_message(message.channel, 'Say hello')
-        msg = await client.wait_for_message(author=message.author, content='hello')
-        await client.send_message(message.channel, 'Hello.')
-
-
-
-
-
+            
 
 client.run("Your_Token")
